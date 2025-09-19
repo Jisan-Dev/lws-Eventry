b1025e0cec6bedcac4b88ca621fa84ec60f373c0
Jisun696 <istiakkashem35@gmail.com>
2025-09-20 02:29:13 +0600

    Build event list and event card component

---
+++ components/landing/event-card.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function EventCard() {
+  return (
+    <div className="overflow-hidden rounded-md bg-[#242526]">
+      <Image
+        src={"/google-io-2023-1.png"}
+        alt={"google-io-2023-1.png"}
+        className="w-full"
+        width={500}
+        height={500}
+      />
+
+      <div className="p-3">
+        <Link href="/" className="font-bold text-lg">
+          Google I/O Extended
+        </Link>
+        <p className="text-[#9C9C9C] text-sm mt-1">
+          Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh
+        </p>
+        <div className="text-[#737373] text-sm mt-1">
+          <span>1k Interested</span>
+          <span>|</span>
+          <span>40K Going</span>
+        </div>
+
+        {/* <!-- Buttons --> */}
+        <div className="w-full flex gap-4 mt-4">
+          {/* <!-- bg-indigo-600 indicating Active --> */}
+          <button className="w-full bg-indigo-600 hover:bg-indigo-800">Interested</button>
+
+          {/* <!-- bg-green-600 indicating Active --> */}
+          <button className="w-full">Going</button>
+        </div>
+      </div>
+    </div>
+  );
+}
