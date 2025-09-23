3a01b80cd2ec6e84a994013c7d3235f5fe48c5f2
Jisun696 <istiakkashem35@gmail.com>
2025-09-24 03:32:37 +0600

    complete the user registration feature

---
+++ actions/index.js
@@ -0,0 +1,12 @@
+"use server";
+
+const { createUser } = require("@/db/queries");
+const { redirect } = require("next/navigation");
+
+const registerUser = async (formData) => {
+  const user = Object.fromEntries(formData);
+  const created = await createUser(user);
+  redirect("/login");
+};
+
+export { registerUser };
